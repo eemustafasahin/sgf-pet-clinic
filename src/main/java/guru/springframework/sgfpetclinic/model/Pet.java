b186273799de4bf344058b4c6f493a42c9cfba04
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-16 10:22:49 +0300

    Add POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sgfpetclinic/model/Pet.java
@@ -0,0 +1,40 @@
+package guru.springframework.sgfpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birtdate;
+
+    public PetType getPetType()
+    {
+        return petType;
+    }
+
+    public void setPetType(PetType petType)
+    {
+        this.petType = petType;
+    }
+
+    public Owner getOwner()
+    {
+        return owner;
+    }
+
+    public void setOwner(Owner owner)
+    {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirtdate()
+    {
+        return birtdate;
+    }
+
+    public void setBirtdate(LocalDate birtdate)
+    {
+        this.birtdate = birtdate;
+    }
+}
---
+++ src/main/java/guru/springframework/sgfpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sgfpetclinic.model;
+
+public class Vet {
+}

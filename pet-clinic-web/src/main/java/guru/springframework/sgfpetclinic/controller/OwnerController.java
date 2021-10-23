e6d669745b6ba1911b3bce68eb1d1c956964d0b1
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-23 22:07:17 +0300

    add layot, closes #20

--- pet-clinic-web/src/main/java/guru/springframework/sgfpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sgfpetclinic/controller/OwnerController.java
@@ -16,7 +16,7 @@ public class OwnerController {
         m_ownerService = ownerService;
     }
 
-    @RequestMapping({"/", "/index", "/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwners(Model model)
     {
         model.addAttribute("owners", m_ownerService.findAll());

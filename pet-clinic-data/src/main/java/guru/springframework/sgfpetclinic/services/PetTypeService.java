659950f2fcc45ba4f5b87950fba2e957aa077688
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-24 12:40:15 +0300

    add PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sgfpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sgfpetclinic.services;
+
+import guru.springframework.sgfpetclinic.model.PetType;
+
+/**
+ * Created by M.Şahin on 24/10/2021
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
